2d97b43aae85553b3e5ee356e0a01e820980e254
gr413_rered <mrstream378@gmail.com>
2023-02-10 11:50:51 +0700

    1

--- js/main.js
+++ js/main.js
@@ -1,113 +1,71 @@
-let eventBus = new Vue()
 
-Vue.component('task-up',{
-    template:`
-    <div>
-       <p v-if="!tasks.length">There are no  yet.</p>
-    <ul v-else>
-        <li v-for="task in tasks">
-            <p>{{ task.task1 }} <button v-on:click="removeOne">Скрыть</button></p>
-            <p>{{ task.task2 }} <button v-on:click="removeTwo">Скрыть</button></p>
-            <p>{{ task.task3 }} <button v-on:click="removeTree">Скрыть</button></p>
-            <p>{{ task.task4 }} <button v-on:click="removeFour">Скрыть</button></p>
-            </li>
-    </ul> 
-    </div>
-  
-    `,
-    data(){
-        return{
-            tasks: [],
-            toggle1: true
-        }
-
-    },
-    mounted()
-    {
-        eventBus.$on('task-submitted', taskRecon => {
-            this.tasks.push(taskRecon)
-        })
-    },
-    methods: {
-        removeOne() {
-            this.$emit('remove-One'), this.tasks.splice(0,2)
-        },
-        removeTwo() {
-            this.$emit('remove-Two'), this.tasks.splice()
-        },
-        removeTree() {
-            this.$emit('remove-Tree'), this.tasks.splice()
-        },
-        removeFour() {
-            this.$emit('remove-Four'), this.tasks.splice()
-        }
-
-        // remove(){
-        //     console.log(1)
-        //     this.$emit('remove'), this.tasks.splice(-1)
-        // }
-    }
-
-})
-Vue.component('cart-create',{
+Vue.component ('first-list', {
     template: `
-<form class="review-form" @submit.prevent="onSubmit">
-                        <p>
-                            <label for="task1">Задача1:</label>
-                            <input id="task1" v-model="task1"required>
-                        </p>
-                         <p>
-                            <label for="task2">Задача2:</label>
-                            <input id="task2" v-model="task2"required>
-                        </p>
-                         <p>
-                            <label for="task3">Задача3:</label>
-                            <input id="task3" v-model="task3"required>
-                        </p>
-                         <p>
-                            <label for="task4">Задача4:</label>
-                            <input id="task4" v-model="task4"required>
-                        </p>
+            <input type="text" @keyup.enter="addNote" v-model="currentNote">
 
-                        <p> <input type="submit" value="Сохранить" >
-                        </p>
-                    </form>
-`,
-    data() {
-        return {
-            tasks1:[],
-            task1:null,
-            task2:null,
-            task3:null,
-            task4:null
+        <ul>
+            <li v-for="note in notes">
+                <input type="text" v-if="note.isEditing" @keyup.enter="editNote(note.text)" v-model="editValue">
+                <span>{{note.text}}</span>
+                <button @click="removeNote(note.text)">Delete</button>
+                <button @click="changeEditing(note.text)">Edit</button>
+            </li>
+        </ul>
+    `,
 
-        }
-    },
-    methods:{
-        onSubmit() {
-            let taskRecon = {
-                name: this.name,
-                task1: this.task1,
-                task2: this.task2,
-                task3: this.task3,
-                task4: this.task4,
-
-            }
-            eventBus.$emit('task-submitted', taskRecon)
-            this.name = null
-            this.task1 = null
-            this.task2 = null
-            this.task3 = null
-            this.task4 = null
-        },
-
-    },
 })
 let app = new Vue({
     el: '#app',
+    editValue:'',
     data: {
+            currentNote: '',
+            notes: [
+                {
+                    text: "adas",
+                    isCompleted: false,
+                    isEditing: false
+                },
 
-    },
+                {
+                    text: "adasasd",
+                    isCompleted: false,
+                    isEditing: false
+                }
+            ]
+        },
     methods: {
-    }
+                addNote: function(){
+                    this.notes.push({
+                        text: this.currentNote,
+                        isCompleted: false
+
+                    });
+                    this.currentNote='';
+                },
+                removeNote: function(noteText){
+                    this.notes = this.notes.filter(note => {
+                        return note.text !== noteText;
+                    })
+
+                },
+                changeEditing: function (noteText) {
+                    this.editValue = noteText;
+                    this.notes = this.notes.map(
+                        note => {
+                            if(note.text === noteText){
+                                note.isEditing = !note.isEditing;
+                            }
+                            return note;
+                        })
+                },
+                editNote: function (noteText){
+                    this.notes = this.notes.map(note =>{
+                        if(note.text === noteText){
+                            note.isEditing = !note.isEditing;
+                            note.text = this.editValue;
+                        }
+                        return note;
+                    })
+                }
+          },
 })
